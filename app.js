a633156c719e7a3c396e8a6feb0790584b8215eb
manas <parey.manas@gmail.com>
2024-02-01 12:08:09 +0530

    funtional components

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // react.createElement always returns an obj
 const heading = React.createElement("h1",{},"react ke andar se");
 root.render(heading)
+
+// JSX
+// () are used when multiple lines are there
+const a = (<h1>heading exists here
+  <p>paragraph exists here</p>
+</h1>);
+
+// functional component
+
+const HTMLHeadingElement = () => {
+  return (
+    <h1>we are inside a funtional component</h1>    
+  )
+}
+
+root.render(<HTMLHeadingElement/>)
